4fa1bf22f532dad321bfeefd1997cadf92755995
mdmathewdc <mathewdony007@gmail.com>
2021-10-02 10:34:46 +1000

    Changed server response

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: "Hello from server...v2!" });
   });
 
   // All other GET requests not handled before will return our React app
