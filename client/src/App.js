aa090b53048ac980d2a9fdb50316bc2ebe9e8b02
mdmathewdc <mathewdony007@gmail.com>
2021-10-02 13:26:16 +1000

    Edited the API path

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch("/api")
+    fetch("/home")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
--- server/index.js
+++ server/index.js
@@ -10,8 +10,12 @@ const app = express();
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+app.get("/home", (req,res) => {
+    res.json({message: "SyncIt App - Coming soon!"});
+});
+
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server...v2!" });
+    res.json({ message: "API Get Call Success!" });
   });
 
   // All other GET requests not handled before will return our React app
